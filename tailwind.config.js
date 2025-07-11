e07cb95c888a0dc4cdd7a5308321039d45891578
Devran <devranmalik82@gmail.com>
2025-07-11 13:18:11 +0700

    feat: Add initial Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.js
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      // Di sini kita bisa menambahkan warna custom nanti jika perlu
+    },
+  },
+  plugins: [],
+};
